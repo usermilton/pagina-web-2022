a2ea6373ec1e4a146cac185447a0b429dbc52338
Virtual21(12) <1376547@seanti.pe>
2022-11-08 13:05:18 -0500

    senati_tarea

---
+++ resources/views/tareasenati.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+body{
+    background-color: gray;
+}
+h1{
+    text-align: center;
+}
+.contenedor{
+    
+    display: flex;
+    padding:25px;
+    margin-top:3px;
+    
+}
+.text{
+    padding-left: 15px
+}
+    </style>
+</head>
+<body>
+   
+   <h1>platos peruanos</h1>
+   <h3>La gastronomía peruana es una de las más reconocidas en todo el mundo por su combinación de sabores tradicionales y exóticos, que tanto gustan a locales y extranjeros.
+     Te invitamos a conocer en este artículo los 3 platillos típicos del Perú que debes probar:</h3>
+     
+
+     <div class="contenedor">
+     <img src="https://tipsparatuviaje.com/wp-content/uploads/2019/08/pan-de-chicharron.jpg"  alt="pan con chicharron"width="300" height="200">
+
+    <div class="text">
+
+    <h1>pan de chicharron:</h1>
+    <h3 >Comencemos la lista con un rico y popular desayuno: el pan de chicharrón.
+     El pan de chicharrón es uno de los platos típicos peruanos para el desayuno que se come en cualquier región del país.
+     Se trata de un pan tipo hamburguesa (paye, francés o el de tu preferencia) relleno con trozos de chuleta de cerdo, acompañado de cebolla (usualmente morada) cortada en julianas y rodajas de camotes o batatas naranja, con una salsa bien sazonada con cebollas, ajo, achiote y rocotos (ají) picados, marinados previamente en jugo de limón, cilantro, vinagre y sal.
+    El también conocido como sándwich de chicharrón es un plato sencillo, delicioso y práctico para también “matar” los antojos.</h3>
+
+
+
+    </div>
+
+ </div>
+     <h1>cuy chactado</h1>
+     <h3>Hasta este punto te preguntaras! ¿Los cusqueños aman el cuy?
+      La verdad es que si, es un plato muy popular no solo en Cusco y Machu Picchu, si no en todo el Perú, porque tiene un sabor    exquisito y es altamente nutritivo.
+      Para prepararlo, la carne de cuy se fríe en aceite y se suele servir con papas hervidas y maíz. Además, se dice que este tipo de carne ya se consumía hace más de 5000 años en el Perú Antiguo.</h3>
+       <img src="https://www.todoenperu.net/platos-tipicos/wp-content/uploads/2019/04/cuy-chactado-cusqueno.jpg" alt="">
+    
+
+ 
+
+
+</body>
+</html>
