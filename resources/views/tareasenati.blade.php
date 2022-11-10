a6aedcbce22bc2da89f1edd6900746927b348b10
Virtual21(12) <1376547@seanti.pe>
2022-11-10 12:00:32 -0500

    regiones

--- resources/views/tareasenati.blade.php
+++ resources/views/tareasenati.blade.php
@@ -8,6 +8,27 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
 </head>
 <body>
+    <header>
+    <div class="container">
+            <nav class="nav">
+                <div class="menu-toggle">
+                    <i class="fas fa-bars"></i>
+                    <i class="fas fa-times"></i>
+                </div>
+                <a href="#" class="logo">perú</a>
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Inicio</a>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link ">Noticias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link ">Tienda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link ">Contacto</a>
+                    </li>
+    </header>
 
 
 
