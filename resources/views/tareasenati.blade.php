a7cb22bec72cf5513681bbf7571f10276d33b5c8
Virtual21(12) <1376547@seanti.pe>
2022-11-10 22:13:00 -0500

    progreso

--- resources/views/tareasenati.blade.php
+++ resources/views/tareasenati.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <header>
-    <div class="container">
+        <div class="container">
             <nav class="nav">
                 <div class="menu-toggle">
                     <i class="fas fa-bars"></i>
@@ -28,7 +28,101 @@
                     <li class="nav-item">
                         <a href="#" class="nav-link ">Contacto</a>
                     </li>
+                </ul>
+            </nav>
+        </div>
     </header>
+
+    <section class="hero" id="hero">
+        <div class="container">
+            <h2 class="h2-sub">
+                <span class="fil">B</span>ienvenidos
+            </h2>
+            <h1 class="head">las 8 regiones del perú</h1>
+            <div class="he-des">
+                <h5>tarea senati</h5>
+                <a href="#" class="btn cta-btn">Explora</a>
+            </div>
+        </div>
+    </section>
+
+    <section class="dis-sto">
+        <div class="container">
+            <div class="res-info">
+                <div>
+                    <img src="https://www.peru.travel/Contenido/Uploads/desierto-de-sechura_637815065702977836.jpg" alt="">
+                </div>
+            
+                <div class="res-des pad-rig">
+                    <div class="global">
+                        <h2 class="h2-sub">
+                            <span class="fil">D</span>escubre
+                        </h2>
+                        <h1 class="head hea-dark">región Chala</h1>
+                        <div class="circle">
+                            <i class="fas fa-circle"></i>
+                        </div>
+                    </div>
+                    <p>
+                        Se encuentra desde la costa hasta los 500 metros sobre el nivel del mar. Entre sus principales bondades están sus extensos desiertos como el de Sechura, con secciones montañosas y dunas que van desde la frontera con Chile en el sur hasta Tumbes.
+                        Allí, diversas especies sobresalen y brillan como los lobos marinos y pintorescas aves guaneras, entre las que destacan el piquero, el guanay y el pelícano. ¿Cómo es el clima?  Predomina una atmósfera tropical seca en la costa norte y subtropical árida en la parte central y sureña.
+                    </p>
+                    <a href="#" class="btn cta-btn">Nosotros</a>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="taste bt">
+        <div class="container">
+            <div class="global">
+                <h2 class="h2-sub">
+                    <span class="fil">D</span>escubre
+                </h2>
+                <h1 class="head hea-dark">región Yunga</h1>
+                <div class="circle">
+                    <i class="fas fa-circle"></i>
+                </div>
+                <div>
+                    <h1 class="milton">Su elevación entre los 500 hasta los 2300 m s. n. m. abarca los Andes peruanos y una zona de la Amazonía. Aquí, el terreno es principalmente montañoso acompañado de maravillosos valles en donde se hallan plantas como la pitahaya, retama y sábila, además de frutos de chirimoya, lúcuma y palta.
+                     Asimismo, es casa de especies como zorros, vizcachas, culebras, coloridas lagartijas, pumas, el famoso oso de anteojos, tigrillos y peculiares picaflores; estos últimos claves para la preservación de la flora local.</h1>
+                </div>
+              
+            </div>
+        </div>
+    </section>
+
+
+    <section class="disco">
+        <div class="container">
+            <div class="res-info">
+                <div class="res-des">
+                    <div class="global">
+                        <h2 class="h2-sub">
+                            <span class="fil">D</span>escubre
+                        </h2>
+                        <h1 class="head hea-dark">región Quechua</h1>
+                        <div class="circle">
+                            <i class="fas fa-circle"></i>
+                        </div>
+                    </div>
+                    <p>
+                        Ofrece un clima diverso, desde un ambiente templado hasta frío en diferentes etapas del año, incluso llegando a fuertes lluvias. ¿Cuáles son las principales bondades que presentan sus suelos y qué animales y plantas convergen en sus tierras?
+                        Su piso altitudinal, que oscila entre los 2300 a 3500 m s. n. m., destaca por sus montañas con alisos, al igual que cuenta con aves como águilas, observadores halcones, ovinos y representativos camélidos (vicuñas y alpacas).
+                    </p>
+                    <a href="#" class="btn cta-btn">nosotros</a>
+                </div>
+                <div class="image-group pad-rig">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX5JIdTDNIgnVK8LZo2ZtDboGSqPlP6A4lIw&usqp=CAU" alt="">
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+
 
 
 
